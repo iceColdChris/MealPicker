831361db8985fe16ccd257ccbd8cdbcb1a657bbb
Chris Fahlin <chris.fahlin@tee2.org>
2017-10-11 12:15:45 -0700

    Got the basic functionality from the devslopes tutorial video, I have some ideas of where to take this app. Will add onto it

--- app/src/main/java/com/cfahlin/mealpicker/MainActivity.kt
+++ app/src/main/java/com/cfahlin/mealpicker/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.cfahlin.mealpicker
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val foodList = arrayListOf("Chinese", "Burgers", "Pizza", "Fast Food")
+    private val random = Random()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        decideButton.setOnClickListener {
+            selectedFoodText.text = foodList[random.nextInt(foodList.count())]
+        }
+
+        addPlaceBtn.setOnClickListener {
+            if(!addFoodText.text.isBlank()) {
+                foodList.add(addFoodText.text.toString())
+                addFoodText.text.clear()
+            }
+        }
     }
 }
