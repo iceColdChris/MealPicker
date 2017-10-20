86efca79b0b9418db3f918361a9e0fdeefa57e70
iceColdChris <chris_fahlin@comcast.net>
2017-10-19 18:20:16 -0700

    Added google maps functionality, checks for permissions

--- app/src/main/java/com/cfahlin/mealpicker/MainActivity.kt
+++ app/src/main/java/com/cfahlin/mealpicker/MainActivity.kt
@@ -1,7 +1,15 @@
 package com.cfahlin.mealpicker
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.cfahlin.mealpicker.R.id.action_map
+import com.cfahlin.mealpicker.R.id.action_settings
+import com.cfahlin.mealpicker.R.menu.top_menu
+import com.reqica.drilon.androidpermissionchecklibrary.CheckPermission
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -14,6 +22,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        checkPermissions()
+
+        my_toolbar.setTitleTextColor(Color.WHITE)
+        setSupportActionBar(my_toolbar)
+
+
         decideButton.setOnClickListener {
             selectedFoodText.text = foodList[random.nextInt(foodList.count())]
         }
@@ -24,5 +38,39 @@ class MainActivity : AppCompatActivity() {
                 addFoodText.text.clear()
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(top_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        if (item != null) {
+            return when(item.itemId) {
+                action_map -> {
+                    val mapIntent = Intent(this, MapsActivity::class.java)
+                    startActivity(mapIntent)
+                    true
+                }
+
+                action_settings->{
+
+                    true
+                }
+                else -> {
+                    super.onOptionsItemSelected(item)
+                }
+            }
+        }
+        return false
+    }
+
+    private fun checkPermissions() {
+        val check  = CheckPermission(this)
+        check.checkOne(android.Manifest.permission.ACCESS_FINE_LOCATION, null)
+
     }
 }
---
+++ app/src/main/java/com/cfahlin/mealpicker/MapsActivity.kt
@@ -0,0 +1,108 @@
+package com.cfahlin.mealpicker
+
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.location.Criteria
+import android.location.LocationManager
+import android.os.Build
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var mLocationManager : LocationManager
+    private lateinit var provider : String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+
+        mLocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        provider = mLocationManager.getBestProvider(Criteria(), true)
+
+        my_toolbar.setTitleTextColor(Color.WHITE)
+        my_toolbar.setTitle(R.string.title_activity_maps)
+        setSupportActionBar(my_toolbar)
+
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        if ( Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission( applicationContext, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission( applicationContext, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+
+        mMap.isMyLocationEnabled = true
+        var location = mLocationManager.getLastKnownLocation(provider)
+        val latitude = location.latitude
+        val longitude = location.longitude
+        val latLng = LatLng(latitude, longitude)
+
+        val yourLocation = CameraUpdateFactory.newLatLngZoom(latLng, 20f)
+        mMap.animateCamera(yourLocation)
+
+
+
+
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.map_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        if (item != null) {
+            return when(item.itemId) {
+                R.id.action_main -> {
+                    val mainIntent = Intent(this, MainActivity::class.java)
+                    startActivity(mainIntent)
+                    true
+                }
+
+                R.id.action_settings ->{
+
+                    true
+                }
+                else -> {
+                    super.onOptionsItemSelected(item)
+                }
+            }
+        }
+        return false
+    }
+}
