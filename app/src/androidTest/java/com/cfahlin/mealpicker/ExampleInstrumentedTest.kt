b09af29783678871f86d4cbd361c802241a22624
Christopher Fahlin <chris_fahlin@comcast.net>
2018-03-01 18:26:39 -0800

    The tests seem to fail when they should, this should be the end of the spam CI Pushes. Yay!

--- app/src/androidTest/java/com/cfahlin/mealpicker/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/cfahlin/mealpicker/ExampleInstrumentedTest.kt
@@ -19,7 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-//        assertEquals("com.cfahlin.mealpicker", appContext.packageName)
-        assertTrue(false)
+        assertEquals("com.cfahlin.mealpicker", appContext.packageName)
     }
 }
